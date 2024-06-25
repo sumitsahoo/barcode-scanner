571e6a5d467bebcfb1d6349af3d64d2af9eff843
Sumit Sahoo <sumitsahoo1988@gmail.com>
2024-06-25 20:22:47 +0530

    feat: Add @tsparticles/react and @tsparticles/slim dependencies for particle effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,100 @@
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+import { useEffect, useState } from "react";
 import BarcodeScanner from "./components/BarcodeScanner";
 
-function App() {
-  return <BarcodeScanner />;
-}
+const App = () => {
+  const [_init, setInit] = useState(false);
+  // this should be run only once per application lifetime
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      //await loadAll(engine);
+      //await loadFull(engine);
+      await loadSlim(engine);
+      //await loadBasic(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  const options = {
+    background: {
+      color: "#eceff1",
+    },
+    particles: {
+      number: {
+        value: 80,
+        density: {
+          enable: true,
+          area: 800,
+        },
+      },
+      color: {
+        value: ["#78909C"],
+      },
+      shape: {
+        type: "circle",
+      },
+      opacity: {
+        value: 1,
+      },
+      size: {
+        value: { min: 1, max: 8 },
+      },
+      links: {
+        enable: true,
+        distance: 150,
+        color: "#78909C",
+        opacity: 0.4,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 1,
+        direction: "none",
+        random: false,
+        straight: false,
+        outModes: "out",
+      },
+    },
+    interactivity: {
+      events: {
+        onHover: {
+          enable: true,
+          mode: "grab",
+        },
+        onClick: {
+          enable: true,
+          mode: "push",
+        },
+      },
+      modes: {
+        grab: {
+          distance: 140,
+          links: {
+            opacity: 1,
+          },
+        },
+        push: {
+          quantity: 4,
+        },
+      },
+    },
+  };
+
+  return (
+    <div className="App">
+      <Particles options={options} init={particlesLoaded} />
+      <BarcodeScanner />
+    </div>
+  );
+};
 
 export default App;
