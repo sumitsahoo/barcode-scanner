42deae4a88c5538a20aac0796ff63701c4866f6e
Sumit Sahoo <sumitsahoo1988@gmail.com>
2025-06-18 18:37:26 +0530

    feat: add titles to SVG icons for improved accessibility

--- src/components/icons/IconRotateCamera.jsx
+++ src/components/icons/IconRotateCamera.jsx
@@ -13,16 +13,17 @@ const IconRotateCamera = ({ className = "", ...props }) => (
         xmlns="http://www.w3.org/2000/svg"
         {...props}
     >
-        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+        <title>Rotate Camera Icon</title>
+        <g id="SVGRepo_bgCarrier" strokeWidth="0" />
+        <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" />
         <g id="SVGRepo_iconCarrier">
             <path
                 d="M14.89 5.0799C14.02 4.8199 13.06 4.6499 12 4.6499C7.20996 4.6499 3.32996 8.5299 3.32996 13.3199C3.32996 18.1199 7.20996 21.9999 12 21.9999C16.79 21.9999 20.67 18.1199 20.67 13.3299C20.67 11.5499 20.13 9.8899 19.21 8.5099"
-                stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-            <path d="M16.13 5.32L13.24 2" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
-                stroke-linejoin="round"></path>
-            <path d="M16.13 5.32007L12.76 7.78007" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
-                stroke-linejoin="round"></path>
+                stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M16.13 5.32L13.24 2" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"
+                strokeLinejoin="round" />
+            <path d="M16.13 5.32007L12.76 7.78007" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"
+                strokeLinejoin="round" />
         </g>
     </svg>
 );
